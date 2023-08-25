f29bccfb1c6d37df1961d752dba37fb75bb447a4
clara02mc <maria.cvm172002@gmail.com>
2023-08-24 22:48:48 -0300

    primeiro commit

---
+++ src/Main/Program.java
@@ -0,0 +1,30 @@
+package Main;
+
+import java.util.Scanner;
+
+import entities.Counter;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		System.out.println("Digite uma frase: ");
+		String palavra = sc.nextLine();
+		
+		int Quantidadedepalavras = Counter.ContadordePalavras(palavra);
+		System.out.printf("Numero total de palavras nesta frase: " + Quantidadedepalavras);
+
+		//Counter.encontrarPalavras(palavra);
+		System.out.println(Counter.encontrarPalavras( palavra));
+
+      sc.close();
+	}
+
+
+	}
+
+
