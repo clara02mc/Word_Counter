a4a9106ecdbd3aee7297ee61301a629109d681e5
felippemozer <felippemozer22@gmail.com>
2023-08-28 21:34:44 -0300

    commit do boy

--- src/Main/Program.java
+++ src/Main/Program.java
@@ -7,24 +7,16 @@ import entities.Counter;
 public class Program {
 
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
-		
-		
-		
-		System.out.println("Digite uma frase: ");
+
+		System.out.printf("Digite uma frase: ");
 		String palavra = sc.nextLine();
-		
-		int Quantidadedepalavras = Counter.ContadordePalavras(palavra);
-		System.out.printf("Numero total de palavras nesta frase: " + Quantidadedepalavras);
 
-	
-		System.out.println(Counter.encontrarPalavras( palavra));
+		int quantidadeDePalavras = Counter.contadorPalavras(palavra);
+		System.out.println("Numero total de palavras nesta frase: " + quantidadeDePalavras);
 
-      sc.close();
+		System.out.println(Counter.encontrarPalavras(palavra));
+
+      	sc.close();
 	}
-
-
-	}
-
-
+}
--- src/entities/Counter.java
+++ src/entities/Counter.java
@@ -5,26 +5,23 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Counter {
-	
-	  public static int ContadordePalavras(String palavra) {
-      	String[] p = palavra.split("\\s+");
-      	return p.length;
-	  }      	
-	  
-	  public static List<String> encontrarPalavras(String frase) {
-	        List<String> palavras = new ArrayList<>();
-	        if (frase == null || frase.isEmpty()) {
-	            return palavras;
-	        }
 
-	        StringTokenizer tokenizer = new StringTokenizer(frase);
-	        while (tokenizer.hasMoreTokens()) {
-	            palavras.add(tokenizer.nextToken());
-	        }
-
-	        return palavras;
-	    }
+	public static int contadorPalavras(String palavra) {
+		String[] p = palavra.split("\\s+");
+		return p.length;
 	}
+	 
+	public static List<String> encontrarPalavras(String frase) {
+		List<String> palavras = new ArrayList<>();
+		if (frase == null || frase.isEmpty()) {
+			return palavras;
+		}
 
-	
+		StringTokenizer tokenizer = new StringTokenizer(frase);
+		while (tokenizer.hasMoreTokens()) {
+			palavras.add(tokenizer.nextToken());
+		}
 
+		return palavras;
+	}
+}
